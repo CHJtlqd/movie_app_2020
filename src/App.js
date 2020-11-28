3e0bb648026f43ac365e711d9ef6fbd104843c43
tlqd <tlqd123@outlook.com>
2020-11-29 00:20:28 +0900

    20201129 react study with nomad coder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const movieLike = [
+//   { id: 1, title: "피자", rating: 5 },
+//   { id: 2, title: "치킨", rating: 3 },
+// ];
+
+// // props 내부에 title, somthing, ... 이 있음
+// // {title}의 형식으로 사용가능
+// function Movie({ title, rating }) {
+//   return (
+//     <div>
+//       {title},{rating}
+//     </div>
+//   );
+// }
+
+// Movie.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   rating: PropTypes.number.isRequired,
+// };
+
+// function renderMovie(dish) {
+//   console.log(dish);
+//   return <Movie key={dish.id} title={dish.title} rating={dish.rating} />;
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       Hello!!!
+//       {movieLike.map(renderMovie)}
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  render() {
+    return <h1>Im a class {this.state.count}</h1>;
+  }
 }
 
 export default App;
